a3248ab8cbf1558b538789f8438dac95c6160e9e
Moutz Dahhan <mdahhan4@outlook.com>
2022-02-24 09:50:08 -0500

    Ai Paddle Created

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -7,23 +7,31 @@ class Game extends Phaser.Scene
 {
 	init()
 	{
+		this.paddleRightVelocity = new Phaser.Math.Vector2(0,0)
+	}
+	preload()
+	{
 		
 	}
 
 	create()
 	{
-		const ball = this.add.circle(400, 250, 10, 0xffffff, 1)
-		this.physics.add.existing(ball)
-		ball.body.setBounce(1,1)
+		this.ball = this.add.circle(400, 250, 10, 0xffffff, 1)
+		this.physics.add.existing(this.ball)
+		this.ball.body.setBounce(1,1)
 		
-		ball.body.setCollideWorldBounds(true, 1, 1)
+		this.ball.body.setCollideWorldBounds(true, 1, 1)
 
-		ball.body.setVelocity(-200, 0)
+		this.ball.body.setVelocity(Phaser.Math.Between(-200, 200), Phaser.Math.Between(-200, 200))
 
 		this.paddleLeft = this.add.rectangle(50, 250, 30, 100, 0xffffff, 1)
 		this.physics.add.existing(this.paddleLeft, true)
 
-		this.physics.add.collider(this.paddleLeft, ball)
+		this.paddleRight = this.add.rectangle(750, 250, 30, 100, 0xffffff, 1)
+		this.physics.add.existing(this.paddleRight, true)
+
+		this.physics.add.collider(this.paddleLeft, this.ball)
+		this.physics.add.collider(this.paddleRight, this.ball)
 
 		this.cursors = this.input.keyboard.createCursorKeys()
 
@@ -43,6 +51,32 @@ class Game extends Phaser.Scene
 			this.paddleLeft.y += 10
 			body.updateFromGameObject()
 		}
+
+		const diff = this.ball.y - this.paddleRight.y
+		if (Math.abs(diff) < 10)
+		{
+			return
+		}
+		const aiSpeed = 3
+		if (diff < 0)
+		{
+			this.paddleRightVelocity.y = -aiSpeed
+			if (this.paddleRightVelocity.y < -10)
+			{
+				this.paddleRightVelocity.y = -10
+			} 
+		}
+		else if (diff > 0)
+		{
+			this.paddleRightVelocity.y = aiSpeed
+			if (this.paddleRightVelocity.y < 10)
+			{
+				this.paddleRightVelocity.y = 10
+			} 
+		}
+
+		this.paddleRight.y += this.paddleRightVelocity.y
+		this.paddleRight.body.updateFromGameObject()
 	}
 }
 
