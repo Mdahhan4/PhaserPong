aa7682885e556ec1ddd01ee7a78921af5949c250
Moutz Dahhan <mdahhan4@outlook.com>
2022-02-24 06:38:30 -0500

    Paddle can now be moved using cursor keys.

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -14,16 +14,35 @@ class Game extends Phaser.Scene
 	{
 		const ball = this.add.circle(400, 250, 10, 0xffffff, 1)
 		this.physics.add.existing(ball)
-		ball.body.setBounce(1, 1)
+		ball.body.setBounce(1,1)
 		
 		ball.body.setCollideWorldBounds(true, 1, 1)
 
 		ball.body.setVelocity(-200, 0)
 
-		const paddleLeft = this.add.rectangle(50, 250, 30, 100, 0xffffff, 1)
-		this.physics.add.existing(paddleLeft, true)
+		this.paddleLeft = this.add.rectangle(50, 250, 30, 100, 0xffffff, 1)
+		this.physics.add.existing(this.paddleLeft, true)
 
-		this.physics.add.collider(paddleLeft, ball)
+		this.physics.add.collider(this.paddleLeft, ball)
+
+		this.cursors = this.input.keyboard.createCursorKeys()
+
+	}
+
+	update()
+	{
+		const body = this.paddleLeft.body
+
+		if(this.cursors.up.isDown)
+		{
+			this.paddleLeft.y -= 10
+			body.updateFromGameObject()
+		}
+		else if (this.cursors.down.isDown)
+		{
+			this.paddleLeft.y += 10
+			body.updateFromGameObject()
+		}
 	}
 }
 
